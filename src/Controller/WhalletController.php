3b786f2364b5e912707568800cdd4cd3cd6a9c2d
pruebasepayco <ricardo.saldarriaga@payco.co>
2020-05-31 20:14:58 -0500

    add consult

--- src/Controller/WhalletController.php
+++ src/Controller/WhalletController.php
@@ -15,10 +15,6 @@ use Firebase\JWT\JWT;
 
 class WhalletController extends AbstractController
 {
-    public $key;
-    public function __construct(){
-        $this->key = '34rsdad52';
-    }
     private function resjson($data){
         //Serializar datos con servicio serializer
         $json = $this->get('serializer')->serialize($data, 'json');
@@ -167,15 +163,13 @@ class WhalletController extends AbstractController
                                     'token' => $token,
                                     'id_session' => $id_session
                                 ];
-
                 }else{
                     $data = [
                         'status' => 'error',
                         'code' => 400,
                         'message' => 'no tiene saldo suficiente para reaizar el pago!',
-                        'saldo actual'=>  $saldo_whallet
+                        'saldo actual'=>  $saldo_whallet." $"
                     ];
-
                 }
                  
                 }
@@ -186,8 +180,6 @@ class WhalletController extends AbstractController
                         'message' => 'authentificacion fallida!'
                     ];
                  }
-
-                
              }
          }
          }else{
@@ -201,7 +193,6 @@ class WhalletController extends AbstractController
      }
 
      public function confirmar(Request $request, JwtAuth $jwt_auth ){
-       // var_dump('llego');
         //recoger los datos por post
        $params = json_decode($request->getContent(), true);
           //recojer el tocken
@@ -280,7 +271,65 @@ class WhalletController extends AbstractController
          }
                
         return $this->resjson($data);
-
     }
+
+    public function consultar(Request $request, JwtAuth $jwt_auth ){
+        //recoger los datos por post
+         $params = json_decode($request->getContent(), true);
+          //recojer el tocken
+         $tokenArray= $params[0];
+         $texto = preg_replace('([^A-Za-z0-9 ])', '', $tokenArray);
+         $cadena_limpia = str_replace('"\"', '', $tokenArray);
+         $token = $cadena_limpia;
+         //comprar si es correcto el token
+         $authCheck = $jwt_auth->checkToken($token);
+         if($authCheck){ 
+         //recoger el objeto del usuario identificado
+         $identity = $jwt_auth->checkToken($token, true);
+         //comprombar y validar datos
+         if(!empty($params))
+         {
+             $user_id = ($identity->sub != null) ? $identity->sub : null;
+             $documento = $params['documento'];
+             $celular = $params['celular'];
+             if(!empty($user_id)){
+                $em = $this->getDoctrine()->getManager(); 
+                $usuario = $this->getDoctrine()->getRepository(User::class)->findOneBy([
+                    'id'=>$user_id
+                ]);
+                $documento = $this->getDoctrine()->getRepository(User::class)->findOneBy([
+                    'documento'=>$documento
+                ]);
+                $celular = $this->getDoctrine()->getRepository(User::class)->findOneBy([
+                    'celular'=>$celular
+                ]);
+                if($documento && $celular && $usuario){
+                    foreach ($usuario->getWhallets() as $whallet) {
+                        $saldo_whallet = $whallet->getSaldo();
+                        $id_whallet=$whallet->getId();
+                       }
+                       $data = [
+                        'status' => 'success',
+                        'code' => 200,
+                        'saldo disponible' => $saldo_whallet." $"
+                    ];
+                }else{
+                    $data = [
+                        'status' => 'error',
+                        'code' => 400,
+                        'message' => 'la cedula o celular no se encuentran registrados!'
+                    ];
+                }
+             }
+         }
+         }else{
+             $data = [
+                 'status' => 'error',
+                 'code' => 400,
+                 'message' => 'authentificacion fallida!'
+             ];
+         }
+         return $this->resjson($data);
+     }
 
 }
